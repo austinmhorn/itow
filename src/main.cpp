f4bc1342655c24189b703d9f1e6c082b7ca00c47
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2023-08-11 17:42:52 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,3 @@ int main(int argc, const char **argv) {
     
     return (EXIT_SUCCESS);
 }
-
-
-
-
-
-/*
-for (int i = 0; i < 1000000; i++)
-{
-    std::string input = std::to_string(i);
-    
-    printRawInput(input);
-    
-    ntot converter(input);
-    
-    converter.printStats();
-    converter.print();
-}
-*/
